05cb2a9d8d4cbc85a240ee58378b0baea32619f0
Atindra Ghosh <atindraghosh2017@gmail.com>
2025-03-22 09:15:39 +0530

    app.js modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import "./index.css";
+import Home from "./routes/Home";
+import AboutUs from "./routes/AboutUs";
+import Contact from "./routes/Contact";
+import Project from "./routes/Project";
+import { Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <h1>This is my portfolio</h1>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Project" element={<Project />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/AboutUs" element={<AboutUs />} />
+      </Routes>
+    </>
   );
 }
 
