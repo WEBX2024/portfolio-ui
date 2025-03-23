1a73c758f59e17a30a355e6e1ea6cb645f1b5719
Atindra Ghosh <atindraghosh2017@gmail.com>
2025-03-23 11:23:38 +0530

    navbar updated/hero img file created

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,40 @@
-import { Link } from "react-router-dom"
-import "./Navbar.css"
-
-import React from 'react'
-import { FaBars } from "react-icons/fa"
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+
   return (
     <div className="header">
-        <Link to={"/"}>
-            <h1>Portfolio.</h1>
-        </Link>
-        <ul className="Nav-menu">
-            <li>
-                <Link to={"/"}>Home</Link>
-            </li>
-            <li>
-                <Link to={"/Project"}>Project</Link>
-            </li>
-            <li>
-                <Link to={"/Contact"}>Contact</Link>
-            </li>
-            <li>
-                <Link to={"/AboutUs"}>About Us</Link>
-            </li>
-        </ul>
-        <div className="hamburger">
-            <FaBars size={20} style={{color: "#fff"}} />
-        </div>
+      <Link to={"/"}>
+        <h1>Portfolio.</h1>
+      </Link>
+      <ul className={click ? "Nav-menu active" : "Nav-menu"}>
+        <li>
+          <Link to={"/"}>Home</Link>
+        </li>
+        <li>
+          <Link to={"/Project"}>Project</Link>
+        </li>
+        <li>
+          <Link to={"/Contact"}>Contact</Link>
+        </li>
+        <li>
+          <Link to={"/AboutUs"}>About Us</Link>
+        </li>
+      </ul>
+      <div className="hamburger" onClick={handleClick}>
+        {click ? (
+          <FaTimes size={20} style={{ color: "#fff" }} />
+        ) : (
+          <FaBars size={20} style={{ color: "#fff" }} />
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
