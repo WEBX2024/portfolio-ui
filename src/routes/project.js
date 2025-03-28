0c8d0494d77f20e41df1265e7984e74bfd79cd0b
Atindra Ghosh <atindraghosh2017@gmail.com>
2025-03-28 11:07:02 +0530

    comps and rout updated

--- src/routes/project.js
+++ src/routes/project.js
@@ -3,6 +3,7 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import Heroimg2 from '../components/Heroimg2';
 import Pricing from '../components/Pricing';
+import WorkCard from '../components/WorkCard';
 
 
 const project = () => {
@@ -10,6 +11,7 @@ const project = () => {
     <div>
       <Navbar/>
       <Heroimg2 heading="PROJECTS." text="Some of my recent works"/>
+      <WorkCard/>
       <Pricing/>
       <Footer/>
     </div>
